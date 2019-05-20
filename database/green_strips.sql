8a86fcd02c1af3d3d8e289f6759a4b4131f13a48
nandy_sayantan <sayantan96.nandy@gmail.com>
2019-05-20 21:12:07 +0530

    Database file modified

--- database/green_strips.sql
+++ database/green_strips.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 26, 2019 at 11:01 AM
+-- Generation Time: May 20, 2019 at 05:40 PM
 -- Server version: 10.1.36-MariaDB
 -- PHP Version: 7.1.23
 
@@ -55,6 +55,14 @@ CREATE TABLE `cart` (
   `purchase_type` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Dumping data for table `cart`
+--
+
+INSERT INTO `cart` (`cart_item_id`, `user_id`, `product_id`, `qty`, `purchase_type`) VALUES
+(4, 14, 1, 3, 0),
+(5, 14, 1, 4, 1);
+
 -- --------------------------------------------------------
 
 --
@@ -80,7 +88,9 @@ INSERT INTO `order_details` (`o_id`, `p_id`, `u_id`, `qty`, `purchase_type`, `de
 (1, 2, 1, 11, 0, 'yet to be delivered'),
 (1, 2, 1, 34, 1, 'yet to be delivered'),
 (1, 3, 1, 71, 0, 'yet to be delivered'),
-(2, 2, 1, 212, 1, 'DONE');
+(2, 2, 1, 212, 1, 'DONE'),
+(3, 1, 14, 23, 1, 'yet to be done'),
+(4, 1, 14, 7, 1, 'yet to be done');
 
 -- --------------------------------------------------------
 
@@ -102,30 +112,9 @@ CREATE TABLE `products` (
 --
 
 INSERT INTO `products` (`p_id`, `p_name`, `description`, `image`, `price_ad`, `price_buy`) VALUES
-(1,'Book Store Bag','SIZE : 12.5\" x 10\"\r\nSTRENGTH : Holds upto 3.5 kg.\r\nLIFE SPAN  : 2 months','1.jpg',5,1),
-(2,'Medical Store Bag','SIZE : 9.5\" x 7\"\r\nSTRENGTH : Holds upto 2 kg.\r\nLIFE SPAN  : 2 months','2.jpg',4,1),
-(3,'Pamphlets','SIZE : 7.5\" x 9\"\r\nMATERIAL : Eco - friendly paper.\r\nLIFE SPAN  : 6 months','3.jpg',3,0);
--- --------------------------------------------------------
-
---
--- Table structure for table `responses`
---
-
-CREATE TABLE `responses` (
-  `name` int(11) NOT NULL,
-  `email` int(11) NOT NULL,
-  `u_id` int(11) DEFAULT NULL,
-  `msg` varchar(1000) NOT NULL,
-  `r_id` int(11) NOT NULL,
-  `response` varchar(100) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `responses`
---
-
-INSERT INTO `responses` (`name`, `email`, `u_id`, `msg`, `r_id`, `response`) VALUES
-(12, 12, NULL, 'asdasdsadsascsadsadsadsadasdasd', 1, 'sdsadsadscxczcdfdsfsdfsf');
+(1, 'Book Store Bag', 'SIZE : 12.5\" x 10\"\r\nSTRENGTH : Holds upto 3.5 kg.\r\nLIFE SPAN  : 2 months', '1.jpg', 5, 1),
+(2, 'Medical Store Bag', 'SIZE : 9.5\" x 7\"\r\nSTRENGTH : Holds upto 2 kg.\r\nLIFE SPAN  : 2 months', '2.jpg', 4, 1),
+(3, 'Pamphlets', 'SIZE : 7.5\" x 9\"\r\nMATERIAL : Eco - friendly paper.\r\nLIFE SPAN  : 6 months', '3.jpg', 3, 0);
 
 -- --------------------------------------------------------
 
@@ -161,11 +150,8 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`id`, `f_name`, `l_name`, `organization`, `address`, `email`, `phone`, `password`) VALUES
-(1, 'Sayantan', 'Nandy', 'asdasd', 'SAYANTAN NANDY,, P-111/B, C.I.T. ROAD, BELEGHATA,, Kolkata,, West Bengal,, pincode: 700010', 'xyz@abc.com', 2147483647, '1233'),
-(2, 'Sayantan', 'Nandy', 'asdasd', 'SAYANTAN NANDY,, P-111/B, C.I.T. ROAD, BELEGHATA,, Kolkata,, West Bengal,, pincode: 700010', 'red@erf.com', 2147483647, 'sad'),
-(3, 'abc', 'sabc', 'sdad', '12', 'red@ytx.com', 9094673, 'ssa'),
-(4, 'test1', 'test2', 'kafjslk', 'tasfa', 'test@test.com', 909875475, '123'),
-(5, 'test1', 'BXs', 'dsad', '12, 232', 'xyz@abc.com', 2147483647, '123');
+(1, 'Sayantan', 'Nandy', 'asdasd', 'SAYANTAN NANDY,, P-111/B, C.I.T. ROAD, BELEGHATA,, Kolkata,, West Bengal,, pincode: 700010', 'xyz@abc.com', 2147483647, 'e034fb6b66aacc1d48f445ddfb08da98'),
+(14, 'Sayantan', 'Nandy', 'dsad', 'SAYANTAN NANDY,, P-111/B, C.I.T. ROAD, BELEGHATA,, Kolkata,, West Bengal,, pincode: 700010', 'red@john.com', 703354841, '202cb962ac59075b964b07152d234b70');
 
 --
 -- Indexes for dumped tables
@@ -197,12 +183,6 @@ ALTER TABLE `products`
   ADD PRIMARY KEY (`p_id`);
 
 --
--- Indexes for table `responses`
---
-ALTER TABLE `responses`
-  ADD PRIMARY KEY (`r_id`);
-
---
 -- Indexes for table `review`
 --
 ALTER TABLE `review`
@@ -213,7 +193,9 @@ ALTER TABLE `review`
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`,`email`,`phone`);
+  ADD PRIMARY KEY (`id`,`email`,`phone`),
+  ADD UNIQUE KEY `email` (`email`),
+  ADD UNIQUE KEY `phone` (`phone`);
 
 --
 -- AUTO_INCREMENT for dumped tables
@@ -229,7 +211,7 @@ ALTER TABLE `admin`
 -- AUTO_INCREMENT for table `cart`
 --
 ALTER TABLE `cart`
-  MODIFY `cart_item_id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `cart_item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `products`
@@ -238,16 +220,10 @@ ALTER TABLE `products`
   MODIFY `p_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT for table `responses`
---
-ALTER TABLE `responses`
-  MODIFY `r_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=15;
 
 --
 -- Constraints for dumped tables
